f177eb0dcc112900dba3673dd25dbdca0cb92b5e
Bryan <93528849+Cryptheon@users.noreply.github.com>
2022-12-04 15:58:56 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HPML'
+copyright = '2022, SURF HPML'
+author = 'Bryan Cardenas'
 
 release = '0.1'
 version = '0.1.0'
