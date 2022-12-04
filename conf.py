7a7105a1584f8deafa8588cd504b8694d6e672aa
Bryan <93528849+Cryptheon@users.noreply.github.com>
2022-12-04 23:53:05 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -18,11 +18,12 @@ import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'HPML blog posts'
-copyright = '2022, Bryan Cardenas, SURF HPML'
+copyright = '2022, SURF HPML'
 author = 'Bryan Cardenas Guevara'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.0'
+release = '0.1'
+version = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,13 +31,19 @@ release = 'v1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+#extensions = [
+#	'nbsphinx',
+#	'sphinx.ext.mathjax',
+#	'sphinx_rtd_theme',
+#    'sphinx_gallery.load_style',  # load CSS for gallery (needs SG >= 0.6)
+#]
 extensions = [
-	'nbsphinx',
-	'sphinx.ext.mathjax',
-	'sphinx_rtd_theme',
-    'sphinx_gallery.load_style',  # load CSS for gallery (needs SG >= 0.6)
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -45,18 +52,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
 master_doc = 'index'
 
 highlight_language = 'python3'
@@ -65,4 +65,18 @@ nbsphinx_execute_arguments = [
     "--InlineBackend.figure_formats={'svg', 'pdf'}",
     "--InlineBackend.rc={'figure.dpi': 96}",
 ]
-# pygments_style = 'sphinx'
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
